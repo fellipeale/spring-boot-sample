a361ad2929391375b2776e13ec80f5aac9ef4c7e
Fellipe Alexandre <fellipeale@gmail.com>
2016-07-14 03:11:18 +0000

    Create rest controller

---
+++ src/main/java/br/com/cinq/greet/controller/GreetingController.java
@@ -0,0 +1,33 @@
+package br.com.cinq.greet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cinq.greet.model.Greeting;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private Greeting greeting;
+
+    @Autowired
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void set(@RequestBody Greeting greeting) {
+        this.greeting.setMessage(greeting.getMessage());
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Greeting get() {
+        return greeting;
+    }
+
+}
---
+++ src/main/java/br/com/cinq/greet/model/Greeting.java
@@ -0,0 +1,18 @@
+package br.com.cinq.greet.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeting {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
